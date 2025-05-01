f3e90c2f0748f3f949bef3c05d546e83b435972c
MagicBag-tab <est24347@uvg.edu.gt>
2025-05-01 16:56:48 -0600

    Se crea la clase de BitHandler

---
+++ BitHandler.java
@@ -0,0 +1,15 @@
+/*
+* @file BitHandler.java
+* 
+* @author Sarah Estrada 24347
+* 
+* @date 1/05/2024
+* 
+* @description
+* Programa que maneja un lector de bits y también escribe los bits. También hace las conversiones
+* de ASCII a bit y de bit a ASCII.
+*/
+
+public class BitHandler {
+
+}
