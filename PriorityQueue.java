501b3b1344a4345a03823c5466ec326ac02f41aa
MagicBag-tab <est24347@uvg.edu.gt>
2025-05-01 16:58:50 -0600

    Se crea la clase de PriorityQueue

---
+++ PriorityQueue.java
@@ -0,0 +1,15 @@
+/*
+*@file PriorityQueue.java
+* 
+* @author Sarah Estrada 24347
+* 
+* @date 1/05/2024
+* 
+* @description
+* Programa que realiza el proceso de gestionar los nodos del arbol de huffman
+* segun la frecuencia con la que se usan ciertas letras o simbolos.s
+*/
+
+public class PriorityQueue {
+
+}
