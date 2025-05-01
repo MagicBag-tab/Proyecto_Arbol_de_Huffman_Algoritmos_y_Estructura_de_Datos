ed1ac9d4e817d628c8c1e1e872f4b033bcfcedf7
MagicBag-tab <est24347@uvg.edu.gt>
2025-05-01 16:57:41 -0600

    Se crea la clase de HuffmanNode

---
+++ HuffmanNode.java
@@ -0,0 +1,14 @@
+/*
+* @file HuffamnNode.java
+* 
+* @author Sarah Estrada 24347
+* 
+* @date 1/05/2024
+* 
+* @description
+* Representa los nodos del arbol de huffman
+*/
+
+public class HuffmanNode {
+
+}
