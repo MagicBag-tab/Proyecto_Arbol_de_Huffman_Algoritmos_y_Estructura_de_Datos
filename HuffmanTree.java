f67774060c4b76b05deac25800435b9fde55109a
MagicBag-tab <est24347@uvg.edu.gt>
2025-05-01 16:57:59 -0600

    Se crea la clase de HuffmanTree

---
+++ HuffmanTree.java
@@ -0,0 +1,13 @@
+/*
+* @file HuffmanTree.java
+* 
+* @author Sarah Estrada 24347
+* 
+* @date 1/05/2024
+* 
+* @description
+* Programa que contiene toda la lógica del funcionamiento del arbol de huffman.
+*/
+public class HuffmanTree {
+
+}
