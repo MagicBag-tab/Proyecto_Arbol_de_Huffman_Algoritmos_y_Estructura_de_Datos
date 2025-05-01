935979bd9266f1300812bb3e756caf426b1e6309
MagicBag-tab <est24347@uvg.edu.gt>
2025-05-01 16:58:17 -0600

    Se crea la clase de Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+ * @file Main.java
+ * 
+ * @author Sarah Estrada 24347
+ * 
+ * @date 1/05/2024
+ * 
+ * @description
+ * Programa principal, en donde el usuario elige si desea codificar o
+ * decodificar el archivo
+ */
+public class Main {
+
+}
